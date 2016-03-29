70448b58bee4848dc498227605db8b38578e040d
Ed Mechem <ed@mechem.org>
2016-03-29 14:17:47 -0700

    Add methods (Release 2)

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,13 @@
+# Ed Mechem pairing with Daniel Nguyen
+
+def ourmethod(item1, item2, item3)
+  arr = [item1, item2, item3]
+end
+
+def add_to_array(arr, item)
+  arr << item
+end
+
 drill = []
 p drill
 drill << 1 << 2 << 3 << 4 << 5
@@ -10,9 +20,9 @@ drill.shift
 p drill
 
 if drill.index(6) == nil
-	our_index = "(nil)"
+ our_index = "(nil)"
 else
-	our_index = drill.index(6)
+ our_index = drill.index(6)
 end
 
 p "The index of FixNum 6 is: #{our_index}"
@@ -21,3 +31,8 @@ drill_two = [6, 7, 8]
 
 combined_array = drill + drill_two
 p combined_array
+
+
+p ourmethod("apple", "orange", "banana")
+p add_to_array(["one", "two", "three"], 5)
+p add_to_array([],5)
