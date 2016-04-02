558b6c08c7dc916fd939c123681c9379cf830989
Ed Mechem <ed@mechem.org>
2016-04-02 12:23:28 -0700

    Initial commit; psuedocode & initial code

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+# Get spy's real name
+
+puts "Hello spy! Enter your real name:"
+name = gets.chomp
+
+# Swap first & last name
+
+fake_name = name.split(' ').reverse.join(' ')
+# p fake_name
+
+
+# Change all the vowels to the next vowel in 'aeiou',
+# and all consonants to next consonant in alphabet
+
+
+
