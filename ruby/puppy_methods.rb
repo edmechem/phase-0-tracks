89eaf0ee477f0fc2c3f86849ae2b1c5d80b69926
Ed Mechem <ed@mechem.org>
2016-04-05 11:26:55 -0700

    Add methods (Release 0)

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,29 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(integer)
+    integer.times { puts "Woof!" }
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
+  
+  def dog_years(integer)
+    integer * 7
+  end
+  
+  def sit
+    puts "*sits*"
+  end
 
 end
 
+# Release 0 
+test = Puppy.new
+p test.fetch("ball")
+p test.speak(5)
+p test.roll_over
+p test.dog_years(2)
+p test.sit
