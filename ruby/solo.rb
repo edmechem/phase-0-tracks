bd192a05bf21d5840c23f7ee18205b27effb6c6e
Ed Mechem <ed@mechem.org>
2016-04-06 22:50:28 -0700

    Initial Commit, Design Pseudocode (Release 0)

---
+++ ruby/solo.rb
@@ -0,0 +1,11 @@
+# 6.4 Solo Challenge - Designing & Implementing a Class
+
+# Class: LED Hula Hoop
+# 	Attribute: Size (string)	("small", "medium", "large")
+# 	Attribute: LED_Color (string)	("red", "green", blue", "multi-color")
+# 	Attribute: Is_Lit (boolean)	(true, false)
+
+# 	Method: Toggle_Lights()			(switches LEDs on or off)
+# 	Method: Change_Color(new_color) (changes LED color)
+# 	Method: Spin					(puts "*spins...*")
+# 	Method: Put_Down				(puts "*puts hoop down.*")
