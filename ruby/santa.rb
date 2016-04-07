5572c3bebfe7a67a1fe6a8c3fb2bb246c9d9b3b5
Ed Mechem <ed@mechem.org>
2016-04-06 19:34:39 -0700

    Add attributes to Santa (Release 1)

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		age = 0
 	end
 
 	def speak
@@ -14,15 +18,52 @@ class Santa
 
 end
 
-santa1 = Santa.new
 
-puts ""
-puts "Hey, Santa - say something!"
-santa1.speak
+# # Release 0
+# santa1 = Santa.new
 
-cookie_type = "graham cracker"
-puts ""
-puts "Here, Santa - enjoy this #{cookie_type} :)"
-santa1.eat_milk_and_cookies(cookie_type)
+# puts ""
+# puts "Hey, Santa - say something!"
+# santa1.speak
 
-puts ""
+# cookie_type = "graham cracker"
+# puts ""
+# puts "Here, Santa - enjoy this #{cookie_type} :)"
+# santa1.eat_milk_and_cookies(cookie_type)
+
+# puts ""
+
+
+# Release 1
+santas = []
+new_santa_data = [
+	["bigender", "Icelandic"],
+	["gender fluid", "black"],
+	["female", "prefer not to say"],
+	["agender", "N/A"],
+	["N/A", "white"],
+	["male", "Peruvian"]
+]
+new_santa_data.each { | data | santas.push( Santa.new(data[0], data[1]) ) }
+puts "Counting instances of Santa..."
+santas.each_index { | idx | puts "    Santa #{idx +1} aka #{santas[idx]}"}
+puts "Done."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
