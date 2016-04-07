8b178b6edcc6a04db59bb9164003d19bc6d4f1d0
Ed Mechem <ed@mechem.org>
2016-04-06 23:08:16 -0700

    Write Class (Release 1)

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,11 +1,43 @@
 # 6.4 Solo Challenge - Designing & Implementing a Class
 
 # Class: LED Hula Hoop
-# 	Attribute: Size (string)	("small", "medium", "large")
-# 	Attribute: LED_Color (string)	("red", "green", blue", "multi-color")
-# 	Attribute: Is_Lit (boolean)	(true, false)
+# 	Attribute: size (string)	"small" or "medium" or "large"
+# 	Attribute: is_lit (boolean)	true, false
+# 	Attribute: colors (array)	["red", "green", "blue", "multi"]
+# 	Attribute: color (string)	(one of the above colors)
 
-# 	Method: Toggle_Lights()			(switches LEDs on or off)
-# 	Method: Change_Color(new_color) (changes LED color)
-# 	Method: Spin					(puts "*spins...*")
-# 	Method: Put_Down				(puts "*puts hoop down.*")
+# 	Method: toggle_lights			(switches LEDs on or off)
+# 	Method: change_color(new_color) (changes LED color)
+# 	Method: spin					(puts "*spins hula hoop...*")
+# 	Method: put_down				(puts "*puts hoop down*")
+
+class Hoop
+	attr_reader :size
+	attr_accessor :is_lit, :color
+
+	def initialize(size)
+		puts "Initializing LED Hula Hoop ..."
+		@size = size
+		@is_lit = false
+		@colors = ["red", "green", "blue", "multi"]
+		@color = "red"
+	end
+
+	def toggle_lights
+		@is_lit = !@is_lit
+	end
+
+	def change_color(new_color)
+		@color = new_color
+	end
+
+	def spin
+		puts "*spins hula hoop...*"
+	end
+
+	def put_down
+		puts "*puts hoop down*"
+	end
+
+end
+
