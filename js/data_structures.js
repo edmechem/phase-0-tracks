43c1a972337273e676aa7bf3d310ad24147a59ef
Ed Mechem <ed@mechem.org>
2016-04-12 14:15:50 -0700

    Initial commit; 'Release 0'

---
+++ js/data_structures.js
@@ -0,0 +1,17 @@
+// Make some arrays
+var colors = ["red", "green", "blue", "violet"]
+var names = ["Edward", "Edwin", "Edmund", "Edgar"]
+
+// Test initial arrays
+console.log(colors);
+console.log(names);
+
+// Push some new values
+colors.push("silver");
+names.push("Eduardo");
+
+// Test new arrays
+console.log("");
+console.log(colors);
+console.log(names);
+
