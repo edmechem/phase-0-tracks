860bb0041b4d3b3d56624c989c0d29612ed3ffcf
Ed Mechem <ed@mechem.org>
2016-04-12 12:58:48 -0700

    Initial commit (Pseudocode 'reverse' function)

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+// Function that takes a string, and reverses it
+function reverse(input) {
+	// Declare empty 'reversed' string that will contain the input reversed
+	// Get length of input; use that as an index loop counter
+	// Counting backwards, for each character
+	//   Add it to 'reversed'
+	// When done, return 'reversed'
+}
+
+
+// Test the function
+var testString = "hello";
+
+// Should print out "olleh"
+console.log(reverse(testString));
