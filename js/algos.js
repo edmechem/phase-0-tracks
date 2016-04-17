958cc5225814bee97c14347f2a15f2fe674fd715
Ed Mechem <ed@mechem.org>
2016-04-16 18:36:51 -0700

    Initial commit; Release 0 pseudocode

---
+++ js/algos.js
@@ -0,0 +1,22 @@
+ // Release 0: Find the Longest Word
+
+ function longestPhrase(arrayOfStrings) {
+ 	// Pseudocode:
+ 	// This function will return the longest phrase
+ 	// If two phrases are identical length,
+ 	// it'll return only the first
+ 	// 
+ 	// Declare string var called longestSoFar, initialize to ""
+ 	// 
+ 	// For each item, see if its length is longer than longestSoFar,
+ 	// if so then push it into longestSoFar
+ 	// 
+ 	// At the end, return longestSoFar
+ }
+
+
+ // Driver code
+
+ // Test the function
+var testArray = ["long phrase","longest phrase","longer phrase"];
+console.log(longestPhrase(testArray));
