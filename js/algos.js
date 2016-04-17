e09ce85b1d4970ce09f3d9ee326a55e27e5fd325
Ed Mechem <ed@mechem.org>
2016-04-16 19:06:44 -0700

    Add Release 1 pseudocode

--- js/algos.js
+++ js/algos.js
@@ -24,9 +24,29 @@
  }
 
 
+// Release 1: Find a Key-Value Match
+
+function findMatch(twoObjects) {
+	// Pseudocode:
+	// Presuming the objects are key/value pairs
+	// Also presuming just two objects
+	// thus, this is very specific code (not general)
+	//
+	// Declare returnValue boolean as false
+	// Declare two objects with the info from these two objects, & compare
+	//   (do I need to? may be able to just compare
+	//   the two objects directly)
+	// 
+	// If anything matches, change returnValue to true
+	// 
+	// At end, return returnValue
+}
+
+
  // Driver code
 
  // Test the function
 var testArray = ["long phrase","longest phrases","longer phrase", "phrases longest"];
 console.log("Our test array is: " + testArray)
 console.log("The longest phrase is: " + longestPhrase(testArray));
+
