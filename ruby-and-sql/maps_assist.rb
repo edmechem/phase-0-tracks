b592bee1ebffcd55d0caf843974c3508a2bec46a
Ed Mechem <ed@mechem.org>
2016-04-24 16:47:37 -0700

    Add Status class & table count methods

--- ruby-and-sql/maps_assist.rb
+++ ruby-and-sql/maps_assist.rb
@@ -48,9 +48,48 @@ create_trips_segments_table_cmd = <<-SQL
   )
 SQL
 
+# Class Status, keeps track of:
+#    Current UI state (are we showing info for Main, Trip, Segment, or Location)
+#    Current count of rows, in each of three tables (to display in UI Menus)
+#    We will only ever create one instance of this class.
+class Status
+
+	attr_reader :trips_count, :segments_count, :locations_count
+	attr_accessor :ui_state
+
+	def initialize(db)
+		@db = db
+		@ui_state = "Main"
+		update_all
+	end
+
+	# Ugh - need to "de-array-ify" the result, *TWICE*,
+	# just to get the actual FixNum
+	def update_trips
+		@trips_count = @db.execute("SELECT COUNT(*) FROM trips")[0][0]
+	end
+
+	def update_segments
+		@segments_count = @db.execute("SELECT COUNT(*) FROM segments")[0][0]
+	end
+
+	def update_locations
+		@locations_count = @db.execute("SELECT COUNT(*) FROM locations")[0][0]
+	end
+
+	def update_all
+		update_trips
+		update_segments
+		update_locations
+	end
+end
 
 
 
+# # Method to count how many rows in each
+# def get_locations(db)
+#   db.execute("SELECT * FROM locations")
+# end
 
 
 
@@ -60,10 +99,24 @@ SQL
 db = SQLite3::Database.new("maps_assist.db")
 
 # Create tables (if they don't already exist)
-db.execute(create_locations_table_cmd)
-db.execute(create_segments_table_cmd)
-db.execute(create_trips_table_cmd)
-db.execute(create_trips_segments_table_cmd)
+# uncomment when done; commented for now for speed
+# db.execute(create_locations_table_cmd)
+# db.execute(create_segments_table_cmd)
+# db.execute(create_trips_table_cmd)
+# db.execute(create_trips_segments_table_cmd)
+
+# Create one & only instance of our "Status" class :)
+status = Status.new(db)
+
+
+# # test code print UI state & count of rows in tables
+# puts "UI State is: #{status.ui_state}"
+# puts "Number of trips: #{status.trips_count} Class: #{status.trips_count.class}"
+# puts "Number of segments: #{status.segments_count} Class: #{status.segments_count.class}"
+# puts "Number of locations: #{status.locations_count} Class: #{status.locations_count.class}"
+
+
+
 
 
 # Main Menu - Ask what user wants to do. Choices are:
