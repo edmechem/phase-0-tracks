d063ecf2a98d8feba3312f94da1af637cd1a4334
Ed Mechem <ed@mechem.org>
2016-04-25 00:20:09 -0700

    Complete top level menu functionality

--- ruby-and-sql/maps_assist.rb
+++ ruby-and-sql/maps_assist.rb
@@ -99,27 +99,71 @@ class Status
 end
 
 
-# Display the various Menu Screens
-def	display_main
+# Display the header that comes at the top of the screen,
+# before the various Menu Screens
+
+def	display_header(ui_state, term_width)
 	Screen.clear
-	puts "Main Menu"
+	puts ""
+	puts "+-----------------------+".center(term_width)
+	puts "|      Maps Assist      |".center(term_width)
+	puts "+-----------------------+".center(term_width)
+	puts ""
+	puts ""
+
+	if ui_state == "Main"	
+		puts (ui_state + " Menu").center(term_width)
+	else
+		puts (ui_state).center(term_width)
+	end
+	puts ""
 end
 
-def	display_trips
-	Screen.clear
-	puts "Trips Menu"
+def	display_menu(ui_state, the_menu, term_width)
+
+	# get widest menu item, to nicely lay them out
+	# left aligned but overall centered
+	widest = 0
+	i = 0
+	while i < the_menu.count
+		if the_menu[i].length > widest
+			widest = the_menu[i].length
+		end
+		i += 1
+	end
+
+	# ok, display the menu
+	the_menu.each{ | item | puts item.ljust(widest).center(term_width)}
 end
 
-def	display_segments
-	Screen.clear
-	puts "Segments Menu"
+
+def display_count(ui_state, term_width, db)
+	
+	# grab count of items
+	count_cmd = "SELECT COUNT(*) FROM " + ui_state.downcase
+	count = db.execute(count_cmd)[0][0]
+
+	# display it
+	puts "There are #{count} #{ui_state.downcase}.".ljust(24).center(term_width)
+	puts ""
+
 end
 
-def	display_locations
-	Screen.clear
-	puts "Locations Menu"
-end
+def display_items(ui_state, term_width, db)
+	
+	# grab count of items
+	count_cmd = "SELECT COUNT(*) FROM " + ui_state.downcase
+	count = db.execute(count_cmd)[0][0]
 
+	# display it
+	puts "There are #{count} #{ui_state.downcase}."
+	puts "Hit any key to continue..."
+	gets
+	# trips = db.execute(
+	# 	"SELECT * FROM trips"
+	# )
+	# trips.each { | trip | puts "Trip info: #{trip}"}
+end
 
 
 
@@ -155,44 +199,161 @@ end
 
 
 # test code, to print UI state & table counts
-puts "UI State is: #{status.ui_state}"
-puts "Number of trips: #{status.trips_count} Class: #{status.trips_count.class}"
-puts "Number of segments: #{status.segments_count} Class: #{status.segments_count.class}"
-puts "Number of locations: #{status.locations_count} Class: #{status.locations_count.class}"
+# puts "UI State is: #{status.ui_state}"
+# puts "Number of trips: #{status.trips_count} Class: #{status.trips_count.class}"
+# puts "Number of segments: #{status.segments_count} Class: #{status.segments_count.class}"
+# puts "Number of locations: #{status.locations_count} Class: #{status.locations_count.class}"
 
-puts ""
+# puts ""
 # puts "Term width: #{term_width}"
 # puts ""
-puts "Hit return to continue..."
-gets.chomp
+# puts "Hit return to continue..."
+# gets.chomp
+
+
+# Declare hash of arrays containing menu items to display
+menus = {
+	"Main" => [
+		"(T)rips",
+		"(S)egments",
+		"(L)ocations",
+		"",
+		"(B)ack to Main Menu",
+		"(Q)uit"
+	],
+	"Trips" => [
+		"(M)ap a trip",
+		"(E)dit a trip",
+		"(D)elete a trip",
+		"",
+		"(N)ew trip",
+		"",
+		"(B)ack to Main Menu",
+		"(Q)uit"
+	],
+	"Segments" => [
+		"(M)ap a segment",
+		"(E)dit a segment",
+		"(D)elete a segment",
+		"",
+		"(N)ew segment",
+		"",
+		"(B)ack to Main Menu",
+		"(Q)uit"
+	],
+	"Locations" => [
+		"(M)ap a location",
+		"(E)dit a location",
+		"(D)elete a location",
+		"",
+		"(N)ew location",
+		"",
+		"(B)ack to Main Menu",
+		"(Q)uit"
+	]
+}
+
+# Declare hash of valid input
+valid_inputs = {
+	"Main" => "tslbq",
+	"Trips" => "mednbq",
+	"Segments" => "mednbq",
+	"Locations" => "mednbq",
+}
+
+# Declare hash of inputs => actions
+actions = {
+	"m" => "Map",
+	"e" => "Edit",
+	"d" => "Delete",
+	"n" => "New",
+}
 
 
 # Main "event loop". Do all the stuff here,
-# until user indicates done by entering Q for Quit.
+# until user indicates done by entering q (for Quit).
 done = false
 until done
-	if status.ui_state == "Main"
-		display_main
-	elsif status.ui_state == "Trips"
-		display_trips
-	elsif status.ui_state == "Segments"
-		display_segments
-	elsif status.ui_state == "Locations"
-		display_locations
-	# If I get to it...
-	# elsif status.ui_state == "About"
-	# 	display_about
+
+	# Select status-appropriate menu
+	the_menu = menus[status.ui_state]
+	
+	# Clear screen & display header
+	display_header(status.ui_state, term_width)
+
+	# If it's not "Main", then show how many of the item in question there are
+	if status.ui_state != "Main"
+		display_count(status.ui_state, term_width, db)
 	end
 
-	# Grab their input. Only one character!
-	puts "What you wanna do?"
+	# Display the appropriate menu, in appropriate width for terminal window
+	display_menu(status.ui_state, the_menu, term_width)
+	
+	# Grab their input.
+	puts ""
+	puts ""
+	puts "Enter your choice then hit return:"
 	input = gets.chomp.byteslice(0)
-	if input == "q"
+
+	# process input & test for validity
+	if input == nil
+		valid = false
+	else
+		input = input.downcase
+		if valid_inputs[status.ui_state].index(input) == nil
+			valid = false
+		else
+			valid = true
+		end
+	end
+	
+	# branch properly on invalid input, or quit, or do the thing
+	if !valid
+		# puts "Valid input for #{status.ui_state} is: '#{valid_inputs[status.ui_state]}'."
+		# puts "You entered #{input}, whose validity is #{valid}."
+		# puts "Please try again. Hit any key to continue..."
+		puts "Invalid input, please try again. Hit any key to continue..."
+		gets
+	elsif input == "q"
 		done = true
 		puts ""
 		puts "Bye!!!"
 		puts ""
+	else
+		# puts "Valid input for #{status.ui_state} is: '#{valid_inputs[status.ui_state]}'"
+		# puts "You entered #{input}, whose validity is #{valid}."
+		# puts "Hit any key to continue..."
+		# gets
+
+		if input == "b"
+			status.ui_state = "Main"
+			# nothing else, we're done
+
+		elsif input == "t"
+			status.ui_state = "Trips"
+			# nothing else, we're done
+
+		elsif input == "s"
+			status.ui_state = "Segments"
+			# nothing else, we're done
+
+		elsif input == "l"
+			status.ui_state = "Locations"
+			# nothing else, we're done
+
+		elsif (input == "m" || input == "e" || input == "d" || input == "n")
+			action = actions[input]
+			puts "#{action} a #{status.ui_state.downcase.chop}".ljust(40).center(term_width)
+			
+			gets
+
+
+
+
+		end
 	end
+
+    
 
 end
 
@@ -206,13 +367,13 @@ end
 # Trips Menu - Display trips, in numbered list.
 # Prompt user to select a trip by typing its number.
 # After they do so, ask what to do. Choices are:
-#     (M)ap it, (C)hange it, (D)elete it, (N)ew Trip, (B)ack to Main
+#     (M)ap it, (E)dit, (D)elete, (N)ew Trip, (B)ack to Main
 #         (if no trips, menu will only show (N)ew Trip & (B)ack to Main)
 
 # Segments Menu - Display segments, in numbered list.
 # Prompt user to select a segment by typing its number.
 # After they do so, ask what to do. Choices are:
-#     (M)ap it, (C)hange it, (D)elete it, (N)ew Segment, (B)ack to Main
+#     (M)ap it, (E)dit, (D)elete, (N)ew Segment, (B)ack to Main
 #         (if no segments, menu will only show (N)ew Segment & (B)ack to Main)
 
 # Locations Menu - Display locations, in numbered list.
