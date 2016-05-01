95611417e0a1852f3fba26551113d4970de284b0
Ed Mechem <ed@mechem.org>
2016-04-30 20:08:03 -0700

    Initial commit; Add Routes (Release 0)

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -9,7 +9,20 @@ db.results_as_hash = true
 # add a query parameter
 # GET /
 get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
+  if params[:name] && params[:age]
+    "#{params[:name]} is #{params[:age]} years old."
+  elsif params[:campus]
+    response = "Students from #{params[:campus]} campus:<br><br>"
+    students = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]])
+    students.each do |student|
+      response << "#{student['name']}, #{student['age']}<br>"
+    end
+    response
+  elsif params.count > 0
+    params.to_s
+  else
+    "Hello world!"
+  end
 end
 
 # write a GET route with
@@ -44,3 +57,26 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# Release 0: Add Routes
+
+# A /contact route that displays an address
+get '/contact' do
+  address = "123 Main St.<br>San Francisco, CA 94101"
+end
+
+# A /great_job route
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+get '/add/:num1/to/:num2' do
+  result = (params[:num1].to_i + params[:num2].to_i).to_s
+  "#{params[:num1]} + #{params[:num2]} = #{result}"
+end
+
